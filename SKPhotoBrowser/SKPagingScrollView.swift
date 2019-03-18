EFRenovation/SKPhotoBrowser

commit c6df44f97cba2ef8ed2575faf0e1900897aa1050

refactoring for 4.0

SKPhotoBrowser/SKPagingScrollView.swift
@@ -14,7 +14,7 @@ class SKPagingScrollView: UIScrollView {
     fileprivate var visiblePages: [SKZoomingScrollView] = []
     fileprivate var recycledPages: [SKZoomingScrollView] = []
     fileprivate weak var browser: SKPhotoBrowser?
-    
+
     var numberOfPhotos: Int {
         return browser?.photos.count ?? 0
     }
@@ -25,16 +25,16 @@ class SKPagingScrollView: UIScrollView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
-        isPagingEnabled = true
-        showsHorizontalScrollIndicator = true
-        showsVerticalScrollIndicator = true
     }
     
     convenience init(frame: CGRect, browser: SKPhotoBrowser) {
         self.init(frame: frame)
         self.browser = browser
-        
+
+        isPagingEnabled = true
+        showsHorizontalScrollIndicator = true
+        showsVerticalScrollIndicator = true
+
         updateFrame(bounds, currentPageIndex: browser.currentPageIndex)
     }
     
@@ -43,7 +43,7 @@ class SKPagingScrollView: UIScrollView {
         visiblePages.removeAll()
         recycledPages.removeAll()
     }
-    
+
     func loadAdjacentPhotosIfNecessary(_ photo: SKPhotoProtocol, currentPageIndex: Int) {
         guard let browser = browser, let page = pageDisplayingAtPhoto(photo) else {
             return
@@ -188,6 +188,16 @@ class SKPagingScrollView: UIScrollView {
             .forEach { captionViews.insert($0.captionView) }
         return captionViews
     }
+    
+    func setControlsHidden(hidden: Bool) {
+        let captionViews = getCaptionViews()
+        let alpha: CGFloat = hidden ? 0.0 : 1.0
+        
+        UIView.animate(withDuration: 0.35,
+                       animations: { () -> Void in
+                        captionViews.forEach { $0.alpha = alpha }
+                       }, completion: nil)
+    }
 }
 
 private extension SKPagingScrollView {
@@ -199,9 +209,9 @@ private extension SKPagingScrollView {
     }
     
     func createCaptionView(_ index: Int) -> SKCaptionView? {
-        if let delegate = self.browser?.delegate {
-            return delegate.captionViewForPhotoAtIndex?(index: index)
-        }
+//        if let delegate = self.browser?.delegate {
+//            return delegate.captionViewForPhotoAtIndex?(index: index)
+//        }
         guard let photo = browser?.photoAtIndex(index), photo.caption != nil else {
             return nil
         }

SKPhotoBrowser.xcodeproj/project.pbxproj
@@ -13,6 +13,8 @@
 		26C97AD71D0EB6A00039F6CB /* SKCacheTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = 26C97AD61D0EB6A00039F6CB /* SKCacheTests.swift */; };
 		26C97AD91D0EB8BB0039F6CB /* SKCacheable.swift in Sources */ = {isa = PBXBuildFile; fileRef = 26C97AD81D0EB8BB0039F6CB /* SKCacheable.swift */; };
 		7643F4B41FE8CE7A00475F6E /* SKActionView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 7643F4B31FE8CE7A00475F6E /* SKActionView.swift */; };
+		7643F4B61FE9F42F00475F6E /* SKPaginationView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 7643F4B51FE9F42F00475F6E /* SKPaginationView.swift */; };
+		7643F4B91FEA4FCF00475F6E /* SKToolbar.swift in Sources */ = {isa = PBXBuildFile; fileRef = 7643F4B81FEA4FCF00475F6E /* SKToolbar.swift */; };
 		8909B5341BC791280060A053 /* SKPhotoBrowser.h in Headers */ = {isa = PBXBuildFile; fileRef = 8909B5331BC791280060A053 /* SKPhotoBrowser.h */; settings = {ATTRIBUTES = (Public, ); }; };
 		8909B5431BC791510060A053 /* SKCaptionView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 8909B53B1BC791510060A053 /* SKCaptionView.swift */; };
 		8909B5441BC791510060A053 /* SKDetectingImageView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 8909B53C1BC791510060A053 /* SKDetectingImageView.swift */; };
@@ -22,7 +24,6 @@
 		8909B5491BC791510060A053 /* SKPhotoBrowser.swift in Sources */ = {isa = PBXBuildFile; fileRef = 8909B5411BC791510060A053 /* SKPhotoBrowser.swift */; };
 		8909B54A1BC791510060A053 /* SKZoomingScrollView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 8909B5421BC791510060A053 /* SKZoomingScrollView.swift */; };
 		8909B54D1BC7916E0060A053 /* SKPhotoBrowser.bundle in Resources */ = {isa = PBXBuildFile; fileRef = 8909B54C1BC7916E0060A053 /* SKPhotoBrowser.bundle */; };
-		890A6F201D5D9E53003B01F0 /* SKToolbar.swift in Sources */ = {isa = PBXBuildFile; fileRef = 890A6F1F1D5D9E53003B01F0 /* SKToolbar.swift */; };
 		8917B1B01D5A13DE000CE1C4 /* SKPhotoBrowserDelegate.swift in Sources */ = {isa = PBXBuildFile; fileRef = 8917B1AF1D5A13DE000CE1C4 /* SKPhotoBrowserDelegate.swift */; };
 		8917B1B41D5A14B0000CE1C4 /* SKButtons.swift in Sources */ = {isa = PBXBuildFile; fileRef = 8917B1B31D5A14B0000CE1C4 /* SKButtons.swift */; };
 		89C24A821D657AD1005F09A9 /* SKPhotoBrowserOptions.swift in Sources */ = {isa = PBXBuildFile; fileRef = 89C24A811D657AD1005F09A9 /* SKPhotoBrowserOptions.swift */; };
@@ -52,6 +53,8 @@
 		26C97AD61D0EB6A00039F6CB /* SKCacheTests.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = SKCacheTests.swift; sourceTree = "<group>"; };
 		26C97AD81D0EB8BB0039F6CB /* SKCacheable.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = SKCacheable.swift; sourceTree = "<group>"; };
 		7643F4B31FE8CE7A00475F6E /* SKActionView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SKActionView.swift; sourceTree = "<group>"; };
+		7643F4B51FE9F42F00475F6E /* SKPaginationView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SKPaginationView.swift; sourceTree = "<group>"; };
+		7643F4B81FEA4FCF00475F6E /* SKToolbar.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SKToolbar.swift; sourceTree = "<group>"; };
 		8909B5301BC791280060A053 /* SKPhotoBrowser.framework */ = {isa = PBXFileReference; explicitFileType = wrapper.framework; includeInIndex = 0; path = SKPhotoBrowser.framework; sourceTree = BUILT_PRODUCTS_DIR; };
 		8909B5331BC791280060A053 /* SKPhotoBrowser.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = SKPhotoBrowser.h; sourceTree = "<group>"; };
 		8909B5351BC791280060A053 /* Info.plist */ = {isa = PBXFileReference; lastKnownFileType = text.plist.xml; path = Info.plist; sourceTree = "<group>"; };
@@ -63,7 +66,6 @@
 		8909B5411BC791510060A053 /* SKPhotoBrowser.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = SKPhotoBrowser.swift; sourceTree = "<group>"; };
 		8909B5421BC791510060A053 /* SKZoomingScrollView.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = SKZoomingScrollView.swift; sourceTree = "<group>"; };
 		8909B54C1BC7916E0060A053 /* SKPhotoBrowser.bundle */ = {isa = PBXFileReference; lastKnownFileType = "wrapper.plug-in"; path = SKPhotoBrowser.bundle; sourceTree = "<group>"; };
-		890A6F1F1D5D9E53003B01F0 /* SKToolbar.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = SKToolbar.swift; sourceTree = "<group>"; };
 		8917B1AF1D5A13DE000CE1C4 /* SKPhotoBrowserDelegate.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = SKPhotoBrowserDelegate.swift; sourceTree = "<group>"; };
 		8917B1B31D5A14B0000CE1C4 /* SKButtons.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = SKButtons.swift; sourceTree = "<group>"; };
 		89C24A811D657AD1005F09A9 /* SKPhotoBrowserOptions.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = SKPhotoBrowserOptions.swift; sourceTree = "<group>"; };
@@ -127,6 +129,7 @@
 		8909B5321BC791280060A053 /* SKPhotoBrowser */ = {
 			isa = PBXGroup;
 			children = (
+				7643F4B31FE8CE7A00475F6E /* SKActionView.swift */,
 				89D0BA461D5994A8002A811B /* SKAnimator.swift */,
 				8917B1B31D5A14B0000CE1C4 /* SKButtons.swift */,
 				26C97AD41D0EB6870039F6CB /* SKCache.swift */,
@@ -136,14 +139,14 @@
 				8909B53D1BC791510060A053 /* SKDetectingView.swift */,
 				8909B53E1BC791510060A053 /* SKIndicatorView.swift */,
 				8CA6C6511CBE76E80054D3C2 /* SKLocalPhoto.swift */,
-				7643F4B31FE8CE7A00475F6E /* SKActionView.swift */,
 				89C24A831D657AFE005F09A9 /* SKPagingScrollView.swift */,
+				7643F4B51FE9F42F00475F6E /* SKPaginationView.swift */,
 				8909B53F1BC791510060A053 /* SKPhoto.swift */,
 				8909B5411BC791510060A053 /* SKPhotoBrowser.swift */,
 				89C24A811D657AD1005F09A9 /* SKPhotoBrowserOptions.swift */,
-				890A6F1F1D5D9E53003B01F0 /* SKToolbar.swift */,
 				8909B5331BC791280060A053 /* SKPhotoBrowser.h */,
 				8917B1AF1D5A13DE000CE1C4 /* SKPhotoBrowserDelegate.swift */,
+				7643F4B81FEA4FCF00475F6E /* SKToolbar.swift */,
 				89D0BA481D59966B002A811B /* SKMesurement.swift */,
 				8909B5421BC791510060A053 /* SKZoomingScrollView.swift */,
 				8909B5351BC791280060A053 /* Info.plist */,
@@ -292,13 +295,14 @@
 			buildActionMask = 2147483647;
 			files = (
 				26C97AD91D0EB8BB0039F6CB /* SKCacheable.swift in Sources */,
+				7643F4B91FEA4FCF00475F6E /* SKToolbar.swift in Sources */,
 				89C24A841D657AFE005F09A9 /* SKPagingScrollView.swift in Sources */,
-				890A6F201D5D9E53003B01F0 /* SKToolbar.swift in Sources */,
 				8909B5441BC791510060A053 /* SKDetectingImageView.swift in Sources */,
 				8909B54A1BC791510060A053 /* SKZoomingScrollView.swift in Sources */,
 				8CA6C6521CBE76E80054D3C2 /* SKLocalPhoto.swift in Sources */,
 				89D0BA471D5994A8002A811B /* SKAnimator.swift in Sources */,
 				210E53ED1C986D3A008DD5E3 /* UIView+Radius.swift in Sources */,
+				7643F4B61FE9F42F00475F6E /* SKPaginationView.swift in Sources */,
 				7643F4B41FE8CE7A00475F6E /* SKActionView.swift in Sources */,
 				8917B1B01D5A13DE000CE1C4 /* SKPhotoBrowserDelegate.swift in Sources */,
 				D2265CCF1F799C75009256CC /* UIApplication+UIWindow.swift in Sources */,

SKPhotoBrowser/SKActionView.swift
@@ -32,6 +32,21 @@ class SKActionView: UIView {
         configureDeleteButton()
     }
     
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        if let view = super.hitTest(point, with: event) {
+            if closeButton.frame.contains(point) || deleteButton.frame.contains(point) {
+                return view
+            }
+            return nil
+        }
+        return nil
+    }
+    
+    func updateFrame(frame: CGRect) {
+        self.frame = frame
+        setNeedsDisplay()
+    }
+
     func updateCloseButton(image: UIImage, size: CGSize? = nil) {
         configureCloseButton(image: image, size: size)
     }
@@ -67,34 +82,6 @@ class SKActionView: UIView {
         
         browser.delegate?.removePhoto?(browser, index: browser.currentPageIndex) { [weak self] in
             self?.browser?.deleteImage()
-        }
-    }
-    
-    @objc func actionButtonPressed(ignoreAndShare: Bool) {
-        if let titles = SKPhotoBrowserOptions.actionButtonTitles {
-            let actionSheetController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-            actionSheetController.addAction(UIAlertAction(title: cancelTitle, style: .cancel))
-            
-            for idx in titles.indices {
-                actionSheetController.addAction(UIAlertAction(title: titles[idx], style: .default, handler: { [unowned self] (_) -> Void in
-                    guard let browser = self.browser else { return }
-                    browser.delegate?.didDismissActionSheetWithButtonIndex?(idx, photoIndex: browser.currentPageIndex)
-                }))
-            }
-            
-            if UI_USER_INTERFACE_IDIOM() == .phone {
-                browser?.present(actionSheetController, animated: true, completion: nil)
-            } else {
-                actionSheetController.modalPresentationStyle = .popover
-                if let popoverController = actionSheetController.popoverPresentationController {
-                    popoverController.sourceView = self
-//                    popoverController.barButtonItem = toolbar.toolActionButton
-                }
-                browser?.present(actionSheetController, animated: true, completion: nil)
-            }
-            
-        } else {
-            browser?.popupShare()
         }
     }
 }

SKPhotoBrowser/SKAnimator.swift
@@ -97,7 +97,7 @@ class SKAnimator: NSObject, SKPhotoBrowserAnimatorDelegate {
         if let resizableImageView = resizableImageView {
             let photo = browser.photoAtIndex(browser.currentPageIndex)
             let contentOffset = scrollView.contentOffset
-            let scrollFrame = scrollView.photoImageView.frame
+            let scrollFrame = scrollView.imageView.frame
             let offsetY = scrollView.center.y - (scrollView.bounds.height/2)
             let frame = CGRect(
                 x: scrollFrame.origin.x - contentOffset.x,

SKPhotoBrowser/SKPaginationView.swift
@@ -6,4 +6,162 @@
 //  Copyright © 2017年 suzuki_keishi. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+private let bundle = Bundle(for: SKPhotoBrowser.self)
+
+class SKPaginationView: UIView {
+    var counterLabel: UILabel!
+    var prevButton: UIButton!
+    var nextButton: UIButton!
+
+    fileprivate weak var browser: SKPhotoBrowser?
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    convenience init(frame: CGRect, browser: SKPhotoBrowser?) {
+        self.init(frame: CGRect(x: 0, y: frame.height - 100, width: frame.width, height: 100))
+        self.browser = browser
+
+        setupApperance()
+        setupCounterLabel()
+        setupPrevButton()
+        setupNextButton()
+
+        update(browser?.currentPageIndex ?? 0)
+    }
+    
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        if let view = super.hitTest(point, with: event) {
+            if counterLabel.frame.contains(point)
+                || prevButton.frame.contains(point)
+                || nextButton.frame.contains(point) {
+                return view
+            }
+            return nil
+        }
+        return nil
+    }
+    
+    func updateFrame(frame: CGRect) {
+        self.frame = CGRect(x: 0, y: frame.height - 100, width: frame.width, height: 100)
+    }
+    
+    func update(_ currentPageIndex: Int) {
+        guard let browser = browser else { return }
+        
+
+        if browser.photos.count > 1 {
+            counterLabel.text = "\(currentPageIndex + 1) / \(browser.photos.count)"
+        } else {
+            counterLabel.text = nil
+        }
+        
+        guard let prevButton = prevButton, let nextButton = nextButton else { return }
+        prevButton.isEnabled = (currentPageIndex > 0)
+        nextButton.isEnabled = (currentPageIndex < browser.photos.count - 1)
+    }
+    
+    func setControlsHidden(hidden: Bool) {
+        let alpha: CGFloat = hidden ? 0.0 : 1.0
+        
+        UIView.animate(withDuration: 0.35,
+                       animations: { () -> Void in self.alpha = alpha },
+                       completion: nil)
+    }
+}
+
+private extension SKPaginationView {
+    func setupApperance() {
+        backgroundColor = .clear
+        clipsToBounds = true
+    }
+
+    func setupCounterLabel() {
+        counterLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
+        counterLabel.center = CGPoint(x: frame.width / 2, y: frame.height / 2)
+        counterLabel.textAlignment = .center
+        counterLabel.backgroundColor = .clear
+        counterLabel.shadowColor = SKToolbarOptions.textShadowColor
+        counterLabel.shadowOffset = CGSize(width: 0.0, height: 1.0)
+        counterLabel.font = SKToolbarOptions.font
+        counterLabel.textColor = SKToolbarOptions.textColor
+        counterLabel.translatesAutoresizingMaskIntoConstraints = true
+        counterLabel.autoresizingMask = [.flexibleBottomMargin,
+                                         .flexibleLeftMargin,
+                                         .flexibleRightMargin,
+                                         .flexibleTopMargin]
+        addSubview(counterLabel)
+    }
+    
+    func setupPrevButton() {
+        guard SKPhotoBrowserOptions.displayBackAndForwardButton else { return }
+        guard browser?.photos.count ?? 0 > 1 else { return }
+        
+        prevButton = SKPrevButton(frame: frame)
+        prevButton.center = CGPoint(x: frame.width / 2 - 100, y: frame.height / 2)
+        prevButton.addTarget(browser, action: #selector(SKPhotoBrowser.gotoPreviousPage), for: .touchUpInside)
+        addSubview(prevButton)
+    }
+    
+    func setupNextButton() {
+        guard SKPhotoBrowserOptions.displayBackAndForwardButton else { return }
+        guard browser?.photos.count ?? 0 > 1 else { return }
+        
+        nextButton = SKNextButton(frame: frame)
+        nextButton.center = CGPoint(x: frame.width / 2 + 100, y: frame.height / 2)
+        nextButton.addTarget(browser, action: #selector(SKPhotoBrowser.gotoNextPage), for: .touchUpInside)
+        addSubview(nextButton)
+    }
+}
+    
+
+class SKPaginationButton: UIButton {
+    let insets: UIEdgeInsets = UIEdgeInsets(top: 13.25, left: 17.25, bottom: 13.25, right: 17.25)
+    
+    func setup(_ imageName: String) {
+        backgroundColor = .clear
+        imageEdgeInsets = insets
+        translatesAutoresizingMaskIntoConstraints = true
+        autoresizingMask = [.flexibleBottomMargin,
+                            .flexibleLeftMargin,
+                            .flexibleRightMargin,
+                            .flexibleTopMargin]
+        contentMode = .center
+        
+        let image = UIImage(named: "SKPhotoBrowser.bundle/images/\(imageName)",
+            in: bundle, compatibleWith: nil) ?? UIImage()
+        setImage(image, for: UIControlState())
+    }
+}
+
+class SKPrevButton: SKPaginationButton {
+    let imageName = "btn_common_back_wh"
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+        setup(imageName)
+    }
+}
+
+class SKNextButton: SKPaginationButton {
+    let imageName = "btn_common_forward_wh"
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+        setup(imageName)
+    }
+}
+

SKPhotoBrowser/SKPhotoBrowser.swift
@@ -25,8 +25,9 @@ open class SKPhotoBrowser: UIViewController {
     fileprivate let animator: SKAnimator = .init()
     
     fileprivate var actionView: SKActionView!
+    fileprivate var paginationView: SKPaginationView!
     fileprivate var toolbar: SKToolbar!
-    
+
     // actions
     fileprivate var activityViewController: UIActivityViewController!
     fileprivate var panGesture: UIPanGestureRecognizer!
@@ -106,6 +107,7 @@ open class SKPhotoBrowser: UIViewController {
         configurePagingScrollView()
         configureGestureControl()
         configureActionView()
+        configurePaginationView()
         configureToolbar()
 
         animator.willPresent(self)
@@ -125,14 +127,19 @@ open class SKPhotoBrowser: UIViewController {
     override open func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         isPerformingLayout = true
-        
-        pagingScrollView.updateFrame(view.bounds, currentPageIndex: currentPageIndex)
-        
-        toolbar.frame = frameForToolbarAtOrientation()
-        
         // where did start
         delegate?.didShowPhotoAtIndex?(self, index: currentPageIndex)
+
+        // toolbar
+        toolbar.frame = frameForToolbarAtOrientation()
         
+        // action
+        actionView.updateFrame(frame: view.frame)
+
+        // paging
+        paginationView.updateFrame(frame: view.frame)
+        pagingScrollView.updateFrame(view.bounds, currentPageIndex: currentPageIndex)
+
         isPerformingLayout = false
     }
     
@@ -177,13 +184,9 @@ open class SKPhotoBrowser: UIViewController {
     
     open func performLayout() {
         isPerformingLayout = true
-        
-        toolbar.updateToolbar(currentPageIndex)
-        
+
         // reset local cache
         pagingScrollView.reload()
-        
-        // reframe
         pagingScrollView.updateContentOffset(currentPageIndex)
         pagingScrollView.tilePages()
         
@@ -239,6 +242,7 @@ public extension SKPhotoBrowser {
             if !isViewActive {
                 pagingScrollView.tilePages()
             }
+            paginationView.update(currentPageIndex)
         }
     }
     
@@ -248,8 +252,7 @@ public extension SKPhotoBrowser {
                 return
             }
             isEndAnimationByToolBar = false
-            toolbar.updateToolbar(currentPageIndex)
-            
+
             let pageFrame = frameForPageAtIndex(index)
             pagingScrollView.jumpToPageAtIndex(pageFrame)
         }
@@ -298,7 +301,7 @@ public extension SKPhotoBrowser {
     }
     
     func areControlsHidden() -> Bool {
-        return toolbar.alpha == 0.0
+        return paginationView.alpha == 0.0
     }
     
     func popupShare(includeCaption: Bool = true) {
@@ -507,7 +510,7 @@ internal extension SKPhotoBrowser {
             if currentPageIndex != 0 {
                 gotoPreviousPage()
             }
-            toolbar.updateToolbar(currentPageIndex)
+            paginationView.update(currentPageIndex)
             
         } else if photos.count == 1 {
             dismissPhotoBrowser(animated: true)
@@ -543,43 +546,42 @@ private extension SKPhotoBrowser {
         view.addSubview(actionView)
     }
 
+    func configurePaginationView() {
+        paginationView = SKPaginationView(frame: view.frame, browser: self)
+        view.addSubview(paginationView)
+    }
+    
     func configureToolbar() {
         toolbar = SKToolbar(frame: frameForToolbarAtOrientation(), browser: self)
         view.addSubview(toolbar)
     }
-    
+
     func setControlsHidden(_ hidden: Bool, animated: Bool, permanent: Bool) {
+        // timer update
         cancelControlHiding()
         
-        let captionViews = pagingScrollView.getCaptionViews()
+        // scroll animation
+        pagingScrollView.setControlsHidden(hidden: hidden)
+
+        // paging animation
+        paginationView.setControlsHidden(hidden: hidden)
+        
+        // action view animation
         actionView.animate(hidden: hidden)
-        UIView.animate(withDuration: 0.35,
-                       animations: { () -> Void in
-                        let alpha: CGFloat = hidden ? 0.0 : 1.0
-                        self.toolbar.alpha = alpha
-                        self.toolbar.frame = hidden ? self.frameForToolbarHideAtOrientation() : self.frameForToolbarAtOrientation()
-                        
-                        captionViews.forEach { $0.alpha = alpha }
-        }, completion: nil)
+        
         if !permanent {
             hideControlsAfterDelay()
         }
         setNeedsStatusBarAppearanceUpdate()
     }
-    
-
 }
 
 // MARK: - UIScrollView Delegate
 
 extension SKPhotoBrowser: UIScrollViewDelegate {
     public func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        guard isViewActive else {
-            return
-        }
-        guard !isPerformingLayout else {
-            return
-        }
+        guard isViewActive else { return }
+        guard !isPerformingLayout else { return }
         
         // tile page
         pagingScrollView.tilePages()
@@ -591,7 +593,7 @@ extension SKPhotoBrowser: UIScrollViewDelegate {
         
         if currentPageIndex != previousCurrentPage {
             delegate?.didShowPhotoAtIndex?(self, index: currentPageIndex)
-            toolbar.updateToolbar(currentPageIndex)
+            paginationView.update(currentPageIndex)
         }
     }
     

SKPhotoBrowser/SKToolbar.swift
@@ -7,3 +7,53 @@
 //
 
 import Foundation
+
+// helpers which often used
+private let bundle = Bundle(for: SKPhotoBrowser.self)
+
+class SKToolbar: UIToolbar {
+    var toolActionButton: UIBarButtonItem!
+    
+    fileprivate weak var browser: SKPhotoBrowser?
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    convenience init(frame: CGRect, browser: SKPhotoBrowser) {
+        self.init(frame: frame)
+        self.browser = browser
+        
+        setupApperance()
+        setupToolbar()
+    }
+}
+
+private extension SKToolbar {
+    func setupApperance() {
+        backgroundColor = .clear
+        clipsToBounds = true
+        isTranslucent = true
+        setBackgroundImage(UIImage(), forToolbarPosition: .any, barMetrics: .default)
+    }
+    
+    func setupToolbar() {
+        toolActionButton = UIBarButtonItem(barButtonSystemItem: .action, target: browser, action: #selector(SKPhotoBrowser.actionButtonPressed))
+        toolActionButton.tintColor = UIColor.white
+        
+        var items = [UIBarButtonItem]()
+        items.append(UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil))
+        if SKPhotoBrowserOptions.displayAction {
+            items.append(toolActionButton)
+        }
+        setItems(items, animated: false)
+    }
+    
+    func setupActionButton() {
+    }
+}
+

commit af5df99f7c6880d46d540a0a2f1e70c39def23fd

Fixed broken page recycling (regression from c6df44f9)

SKPhotoBrowser/SKPagingScrollView.swift
@@ -118,8 +118,7 @@ class SKPagingScrollView: UIScrollView {
         let lastIndex: Int = getLastIndex()
         
         visiblePages
-            .filter({ $0.tag - pageIndexTagOffset < firstIndex })
-            .filter({ $0.tag - pageIndexTagOffset > lastIndex })
+            .filter({ $0.tag - pageIndexTagOffset < firstIndex ||  $0.tag - pageIndexTagOffset > lastIndex })
             .forEach { page in
                 recycledPages.append(page)
                 page.prepareForReuse()